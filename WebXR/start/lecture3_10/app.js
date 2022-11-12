036b86cf68f91d72eff6c67f6131fb467df52f77
Daniel Friedrich <danielfriedrichveloso@gmail.com>
2022-11-12 17:50:28 -0300

    Update app.js

--- WebXR/start/lecture3_10/app.js
+++ WebXR/start/lecture3_10/app.js
@@ -105,7 +105,6 @@ class App{
         this.renderer.xr.enabled = true; 
         
         const self = this;
-        let controller;
         
         function onConnected( event ) {
             if (self.info === undefined){
@@ -141,9 +140,15 @@ class App{
             self.camera.remove(self.ui.mesh);
         }
 
-        const btn = new ARButton(this.renderer, {onSessionStart, onSessionEnd,
-        sessionInit: {optionalFeatures: ['dom-overlay'], domOverlay: {root:
-        document.body}} });
+        const btn = new ARButton( this.renderer, { onSessionStart, onSessionEnd, 
+        sessionInit: { optionalFeatures: [ 'dom-overlay' ], domOverlay: { root:
+        document.body } } } ); 
+        
+        const controller = this.renderer.xr.getController(0);
+        controller.addEventListener('connected', onConnected);
+
+        this.scene.add(controller);
+        this.controller = controller;
 
         this.renderer.setAnimationLoop(this.render.bind(this));
 
